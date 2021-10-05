fa7c0d64fb0715adf8ab980ecf8ae1b433eaf317
Petra Zelic <56545695+pzelic29@users.noreply.github.com>
2021-10-05 15:41:55 +0200

    Add files via upload

---
+++ Nao_project/Zavrsni.pml
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="Zavrsni" format_version="5">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs>
+        <Dialog name="Introduction" src="Introduction/Introduction.dlg" />
+    </Dialogs>
+    <Resources />
+    <Topics>
+        <Topic name="Introduction_enu" src="Introduction/Introduction_enu.top" topicName="Introduction" language="en_US" nuance="enu" />
+    </Topics>
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_de_DE" src="translations/translation_de_DE.ts" language="de_DE" />
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ Nao_project/translations/translation_de_DE.ts
@@ -0,0 +1,138 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+    <context>
+        <name>behavior_1/behavior.xar:/Choice</name>
+        <message>
+            <source>no/no nao/disagree/I disagree</source>
+            <comment>SpeechReco negative</comment>
+            <translation type="vanished">Nein/Nein NAO/Nicht wirklich/Das glaube ich nicht</translation>
+        </message>
+        <message>
+            <source>yes/yes nao/agree/I agree/of course</source>
+            <comment>SpeechReco positive</comment>
+            <translation type="vanished">Ja/Ja NAO/OK/Doch/Ja klar/Auf jeden Fall</translation>
+        </message>
+        <message>
+            <source>help/help me/i don't know</source>
+            <comment>SpeechReco help</comment>
+            <translation type="vanished">Hilfe/die Hilfe/Hilf mir bitte/Ich weiß nicht/Kannst Du mir helfen?/Was mache ich jetzt?/Was kann ich machen?/Wie kann ich das machen?</translation>
+        </message>
+        <message>
+            <source>exit/stop/quit</source>
+            <comment>SpeechReco exit</comment>
+            <translation type="vanished">beenden/verlassen/abbrechen/schließen</translation>
+        </message>
+        <message>
+            <source>repeat/pardon/what/excuse me</source>
+            <comment>SpeechReco repeat</comment>
+            <translation type="vanished">wiederholen/wiederhole/Entschuldigung?/Was?/Wie bitte?</translation>
+        </message>
+        <message>
+            <source>I understood %s. Is that correct?</source>
+            <comment>TTS confirmation</comment>
+            <translation type="vanished">Ich habe %s verstanden. Ist das richtig?</translation>
+        </message>
+        <message>
+            <source>, / or </source>
+            <comment>TTS enumMarks</comment>
+            <translation type="vanished">, / oder </translation>
+        </message>
+        <message>
+            <source>No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</source>
+            <comment>TTS helpEnumChoices</comment>
+            <translation type="vanished">Es wird keine genaue Antwort erwartet. /Du kannst mit %s antworten. /Du kannst zum Beispiel mit %s antworten. /%s ?</translation>
+        </message>
+        <message>
+            <source> you can ask me: %s. </source>
+            <comment>TTS helpEnumDefault</comment>
+            <translation type="vanished"> Desweiteren kannst du %s sagen. </translation>
+        </message>
+        <message>
+            <source>You can also use my tactile sensor to choose the answer./Pau=600 Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</source>
+            <comment>TTS helpTactile</comment>
+            <translation type="vanished">Du kannst auch mithilfe meines Kopfsensors eine Antwort auswählen./\Pau=600\ Drück vorne oder hinten auf meinen Kopfsensor, um durch die möglichen Antworten zu navigieren. Drück auf meinen mittleren Kopfsensor, um deine gewünschte Antwort zu bestätigen.</translation>
+        </message>
+        <message>
+            <source>I did not understand. </source>
+            <comment>TTS notUnderstood</comment>
+            <translation type="vanished">Ich habe nicht verstanden. </translation>
+        </message>
+        <message>
+            <source>No question has been defined, so I cannot repeat it. </source>
+            <comment>TTS noQuestion</comment>
+            <translation type="vanished">Es gibt keine vordefinierte Frage, also kann ich sie nicht wiederholen. </translation>
+        </message>
+        <message>
+            <source>I did not understand. Anims</source>
+            <comment>TTS notUnderstoodAnims</comment>
+            <translation type="vanished">Es ist zu laut hier! /Versuch bitte deutlicher zu sprechen. /Ich verstehe dich nicht gut. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Choice/Choice</name>
+        <message>
+            <source>no/no nao/disagree/I disagree</source>
+            <comment>SpeechReco negative</comment>
+            <translation type="obsolete">Nein/Nein NAO/Nicht wirklich/Das glaube ich nicht</translation>
+        </message>
+        <message>
+            <source>yes/yes nao/agree/I agree/of course</source>
+            <comment>SpeechReco positive</comment>
+            <translation type="obsolete">Ja/Ja NAO/OK/Doch/Ja klar/Auf jeden Fall</translation>
+        </message>
+        <message>
+            <source>help/help me/i don't know</source>
+            <comment>SpeechReco help</comment>
+            <translation type="obsolete">Hilfe/die Hilfe/Hilf mir bitte/Ich weiß nicht/Kannst Du mir helfen?/Was mache ich jetzt?/Was kann ich machen?/Wie kann ich das machen?</translation>
+        </message>
+        <message>
+            <source>exit/stop/quit</source>
+            <comment>SpeechReco exit</comment>
+            <translation type="obsolete">beenden/verlassen/abbrechen/schließen</translation>
+        </message>
+        <message>
+            <source>repeat/pardon/what/excuse me</source>
+            <comment>SpeechReco repeat</comment>
+            <translation type="obsolete">wiederholen/wiederhole/Entschuldigung?/Was?/Wie bitte?</translation>
+        </message>
+        <message>
+            <source>I understood %s. Is that correct?</source>
+            <comment>TTS confirmation</comment>
+            <translation type="obsolete">Ich habe %s verstanden. Ist das richtig?</translation>
+        </message>
+        <message>
+            <source>, / or </source>
+            <comment>TTS enumMarks</comment>
+            <translation type="obsolete">, / oder </translation>
+        </message>
+        <message>
+            <source>No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</source>
+            <comment>TTS helpEnumChoices</comment>
+            <translation type="obsolete">Es wird keine genaue Antwort erwartet. /Du kannst mit %s antworten. /Du kannst zum Beispiel mit %s antworten. /%s ?</translation>
+        </message>
+        <message>
+            <source> you can ask me: %s. </source>
+            <comment>TTS helpEnumDefault</comment>
+            <translation type="obsolete"> Desweiteren kannst du %s sagen. </translation>
+        </message>
+        <message>
+            <source>I did not understand. </source>
+            <comment>TTS notUnderstood</comment>
+            <translation type="obsolete">Ich habe nicht verstanden. </translation>
+        </message>
+        <message>
+            <source>No question has been defined, so I cannot repeat it. </source>
+            <comment>TTS noQuestion</comment>
+            <translation type="obsolete">Es gibt keine vordefinierte Frage, also kann ich sie nicht wiederholen. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Get Localized Text</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hallo</translation>
+        </message>
+    </context>
+</TS>
